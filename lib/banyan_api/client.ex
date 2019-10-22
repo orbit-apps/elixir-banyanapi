f95d7f9e1608e122f2a093a0bec0d30391b7b1d4
Mark Zsombor <markszsombor@gmail.com>
2019-10-22 10:46:27 -0700

    comment for why the dialyzer ignore is needed

--- lib/banyan_api/client.ex
+++ lib/banyan_api/client.ex
@@ -5,6 +5,7 @@ defmodule BanyanAPI.Client do
   alias Neuron.Config
   alias PxUAuth0.AccessToken
 
+  # Neuron specs are screwy, so this needed to match against the HTTPoison Error
   @dialyzer {:nowarn_function, log_and_return!: 1}
   @default_access_token_impl &AccessToken.fetch/0
 
