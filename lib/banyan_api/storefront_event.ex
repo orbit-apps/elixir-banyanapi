7b689d85dcaec4d747532056ea7c8cc93b74f915
Scott Mahr <essmahr@gmail.com>
2019-10-25 14:51:16 -0700

    handle null values for optional storefront event params
    key name fix
    
    fix nil offer handling
    
    add bang to throwy function

--- lib/banyan_api/storefront_event.ex
+++ lib/banyan_api/storefront_event.ex
@@ -9,17 +9,17 @@ defmodule BanyanAPI.StorefrontEvent do
         $app_name: String!,
         $email: String!,
         $shop_myshopify_domain: String!,
+        $type: String!
         $shopify_order: String,
         $recipient_email: String,
-        $type: String!
       ) {
         trackStorefrontEvent(
           app_name: $app_name,
           email: $email,
           shop_myshopify_domain: $shop_myshopify_domain,
+          type: $type
           shopify_order: $shopify_order,
           recipient_email: $recipient_email,
-          type: $type
         ) {
           app_name
         }
@@ -29,10 +29,13 @@ defmodule BanyanAPI.StorefrontEvent do
         app_name: Application.get_env(:banyan_api, :app_name, ""),
         email: shop.settings["email"],
         shop_myshopify_domain: shop.settings["myshopify_domain"],
-        shopify_order: payload |> Map.get(:order, %{}) |> Jason.encode!(),
-        recipient_email: Map.get(payload, :recipient_email, ""),
-        type: type
+        type: type,
+        shopify_order: payload |> Map.get(:shopify_order) |> encode_order_if_exists!(),
+        recipient_email: Map.get(payload, :recipient_email)
       }
     )
   end
+
+  defp encode_order_if_exists!(order) when is_map(order), do: Jason.encode!(order)
+  defp encode_order_if_exists!(_), do: nil
 end
