e19e3e8087bebd716a90a73e4208094df34a560f
Scott Mahr <essmahr@gmail.com>
2019-10-25 13:12:21 -0700

    handle optional storefront event payload params
    start pipe with value

--- lib/banyan_api/storefront_event.ex
+++ lib/banyan_api/storefront_event.ex
@@ -2,7 +2,7 @@ defmodule BanyanAPI.StorefrontEvent do
   alias BanyanAPI.Client
 
   @spec track(map(), String.t(), map()) :: {:ok, %Neuron.Response{}} | {:error, any()}
-  def track(shop, type, %{shopify_order: shopify_order, recipient_email: recipient_email}) do
+  def track(shop, type, payload) do
     Client.query(
       """
       mutation TrackStorefrontEvent(
@@ -29,8 +29,8 @@ defmodule BanyanAPI.StorefrontEvent do
         app_name: Application.get_env(:banyan_api, :app_name, ""),
         email: shop.settings["email"],
         shop_myshopify_domain: shop.settings["myshopify_domain"],
-        shopify_order: Jason.encode!(shopify_order),
-        recipient_email: recipient_email,
+        shopify_order: payload |> Map.get(:order, %{}) |> Jason.encode!(),
+        recipient_email: Map.get(payload, :recipient_email, ""),
         type: type
       }
     )
