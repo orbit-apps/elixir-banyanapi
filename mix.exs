49651d6a4d700a1f1e6298905f07ef2418e5a412
Hez Ronningen <code@hez.ca>
2020-09-22 09:06:24 -0700

    update github actions
    - fix plt caching
    - run dialyzer as test for increased speed

--- mix.exs
+++ mix.exs
@@ -9,7 +9,11 @@ defmodule BanyanAPI.MixProject do
       version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_apps: [:mix, :ex_unit],
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -25,7 +29,7 @@ defmodule BanyanAPI.MixProject do
     [
       # Test and Dev
       {:credo, "~> 1.4.0", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
       # everything else
       {:neuron, "~> 4.1.2"},
       {:pxu_auth0, github: "pixelunion/ex-pxu-auth0", tag: "v0.2.1"}
