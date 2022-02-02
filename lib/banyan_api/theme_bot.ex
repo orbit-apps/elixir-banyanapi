aea61ddf0a24c7b73ed9f45cb514f0cbb8222bc7
Jason Freeman <jason.freeman@pixelunion.net>
2022-02-02 11:25:29 -0800

    Add options to themebot create mutation
    Just adding a map of options here allows us to not have to touch this lib if we want to include more options, we just have to update the schema in banyan.
    
    The option that is being added is to enable conditionally running procedures, specifically the quick order form install in Wholesale for net-new installs, now that QOF will be included in all plans for those merchants

--- lib/banyan_api/theme_bot.ex
+++ lib/banyan_api/theme_bot.ex
@@ -2,24 +2,28 @@ defmodule BanyanAPI.ThemeBot do
   alias BanyanAPI.Client
 
   @spec create(map()) :: {:ok, %Neuron.Response{}} | {:error, any()}
-  def create(%{
-        myshopify_domain: myshopify_domain,
-        theme_id: theme_id,
-        manifest_name: manifest_name
-      }) do
+  def create(
+        %{
+          myshopify_domain: myshopify_domain,
+          theme_id: theme_id,
+          manifest_name: manifest_name
+        } = params
+      ) do
     Client.query(
       """
       mutation CreateThemeBot(
         $myshopify_domain: String!,
         $theme_id: String!,
         $manifest_name: String!,
-        $app_name: String!
+        $app_name: String!,
+        $options: Options
       ) {
         createThemeBot(
           myshopify_domain: $myshopify_domain,
           app_name: $app_name,
           theme_id: $theme_id,
-          manifest_name: $manifest_name
+          manifest_name: $manifest_name,
+          options: $options
       ) {
           manifest_name
         }
@@ -29,7 +33,8 @@ defmodule BanyanAPI.ThemeBot do
         myshopify_domain: myshopify_domain,
         theme_id: theme_id,
         manifest_name: manifest_name,
-        app_name: Application.get_env(:banyan_api, :app_name, "")
+        app_name: Application.get_env(:banyan_api, :app_name, ""),
+        options: Map.get(params, :options, %{})
       }
     )
   end
